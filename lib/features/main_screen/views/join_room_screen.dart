b7f69db681599dfb57dcad93fbe7a2c2e0ee95e2
Gurparvesh Singh <66719267+GurparveshGrewal@users.noreply.github.com>
2024-04-23 15:07:31 +0530

    feat: join and create room class

---
+++ lib/features/main_screen/views/join_room_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe_app/utils/commons/widgets/custom_text.dart';
+import 'package:tictactoe_app/utils/commons/widgets/custom_textfield.dart';
+
+class JoinRoomScreen extends StatefulWidget {
+  static const String routeName = '/join-room';
+  const JoinRoomScreen({super.key});
+
+  @override
+  State<JoinRoomScreen> createState() => _JoinRoomScreenState();
+}
+
+class _JoinRoomScreenState extends State<JoinRoomScreen> {
+  final TextEditingController _roomCodeController = TextEditingController();
+  final TextEditingController _nameCodeController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameCodeController.dispose();
+    _roomCodeController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+              onPressed: Navigator.of(context).pop,
+              icon: const Icon(
+                Icons.close,
+                size: 30,
+                color: Colors.white,
+              )),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CustomText(
+                text: 'Join Room',
+                shadows: [
+                  Shadow(
+                    color: Colors.blue,
+                    blurRadius: 40,
+                  )
+                ],
+                size: 50),
+            SizedBox(
+              height: height * 0.04,
+            ),
+            CustomTextField(
+              hintText: 'enter your nickname',
+              controller: _nameCodeController,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomTextField(
+              hintText: 'enter the room code',
+              controller: _roomCodeController,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe_app/features/main_screen/views/create_room_screen.dart';
+import 'package:tictactoe_app/features/main_screen/views/join_room_screen.dart';
 import 'package:tictactoe_app/features/main_screen/views/main_menu_screen.dart';
 import 'package:tictactoe_app/utils/app_colors.dart';
 
@@ -11,7 +13,12 @@ class MyApp extends StatelessWidget {
       title: 'TicTacToe',
       theme:
           ThemeData.dark().copyWith(scaffoldBackgroundColor: AppColors.bgColor),
-      home: const MainMenuScreen(),
+      routes: {
+        MainMenuScreen.routeName: (context) => const MainMenuScreen(),
+        CreateRoomScreen.routeName: (context) => const CreateRoomScreen(),
+        JoinRoomScreen.routeName: (context) => const JoinRoomScreen(),
+      },
+      initialRoute: MainMenuScreen.routeName,
     );
   }
 }
